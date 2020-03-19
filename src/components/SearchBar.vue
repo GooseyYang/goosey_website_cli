42cd5e726b6b31c9ea46fbbc8682229da65a1334
lisha.yang <lisha.yang@mihoyo.com>
2020-03-19 14:55:25 +0800

    SearchBar 静态代码

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="search__bar">
+    <div class="search__div">
+      <i class="el-icon-search"></i>
+      <input class="search__input" placeholder="搜索关键词" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "SearchBar"
+};
+</script>
+<style lang="scss" scoped></style>
